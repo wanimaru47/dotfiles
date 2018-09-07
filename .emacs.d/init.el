0a8605549ada5e9a760c5aacae25e4c9cd984567
wanimaru47 <wanimaru47@gmail.com>
2018-09-07 12:59:29 +0900

    init

---
+++ .emacs.d/init.el
@@ -0,0 +1,6 @@
+;; don't backup files
+(setq make-backup-files nil)
+(setq auto-save-default nil)
+
+;; Ctrl-H is BackSpace
+(global-set-key "\C-h" 'delete-backward-char)
---
+++ Makefile
@@ -0,0 +1,6 @@
+test:
+	@echo "Hello World!!"
+
+ln:
+	@sh dotfilesLink.sh
+
