76e74ac394eddf63b488e99da75580ad81b22267
Yuta Wanikawa <wanimaru@Maguro.local>
2018-11-02 15:03:28 +0900

    fix ln

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-ln -sf ~/.dotfiles/.emacs ~/.emacs
 
-ln -sf ~/.dotfiles/.gitconfig ~/.gitconfig
-
-ln -sf ~/.dotfiles/.zshrc ~/.zshrc
+ln -sf ~/.dotfiles/emacs ~/.emacs
+ln -sf ~/.dotfiles/gitconfig ~/.gitconfig
+ln -sf ~/.dotfiles/zshrc ~/.zshrc
 
